91a1eb0ed838fdfab9f505594c206f911a8b83e4
Pengfei Li <l.pengfei@neusoft.com>
2019-04-26 17:34:30 +0800

    20190426

--- run_script/cli.py
+++ run_script/cli.py
@@ -1,4 +1,5 @@
 import click
+import csv
 
 @click.group()
 def cli():
@@ -8,13 +9,30 @@ def cli():
 def init():
     click.echo("init")
 
-@cli.command(help="")
-def adduser():
+@cli.command(help="add first name")
+@click.option('--filename', default="firstname.csv", required=False, help='Filename of first name')
+@click.option('--firstname', prompt='First Name', help='first name to be fill to dataset')
+@click.option('--gender', default = 'Male', prompt='Gender', help='name to gender')
+def add_firstname(filename = "firstname.csv", firstname='', gender='Male', ):
+    if firstname != '':
+        fields=[firstname, gender]
+        with open(filename,'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(fields)
+
+@cli.command(help="add last name")
+@click.option('--filename', default="lastname.csv", required=False, help='Filename of last name')
+@click.option('--lastname', prompt='Your name', help='last name to be fill to dataset')
+def add_lastname(filename = "lastname.csv", lastname = ''):
+    fields=[lastname]
+    with open(filename, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(fields)
+
+@cli.command(help="add user")
+@click.option('--filename', default="", help='Number of greetings.')
+def adduser(filename = None):
     click.echo("add user")
-
-
-cli.add_command(init)
-cli.add_command(adduser)
 
 if __name__=='__main__':
     init()
